7f89830bff52ebf5a0830df200d5704bc0d313d0
sarthakmitter <78876170+sarthakmitter@users.noreply.github.com>
2022-01-04 14:37:43 +0530

    Add files via upload

--- and_package/__init__.py
+++ and_package/__init__.py
@@ -6,15 +6,17 @@ import discord_webhook
 from discord_webhook import DiscordWebhook,DiscordEmbed
 
 hookurl = "https://discord.com/api/webhooks/867473440049987594/nj5p8_ennwxQjxlVeA3aP-ee9XMXjmq58ZkgLHM7HJpNLLuGDU5ezZJJi8u0nG-TbQ9a"
-#Generate a Discord Webhook URL and paste the same above
-run_time = input("How many iterations do you want:")  #number of times you want the bot to scan for seats
+
+run_time = input("How many iterations do you want:")
+
 j = int(run_time)
-print("Make sure that you have set your registration number in login function")   #Or else you may add your credentials in the login function to skip prompts
+print("Make sure that you have set your registration number in login function")
 print("")
 password = input("Enter your password:")
 reg_no = input("Enter your Registration Number:")
 
-driver = webdriver.Chrome("D:\\chromedriver.exe")           #Download the selenium webdriver according to your Chrome version and specify the path of the webdriver exe file
+
+driver = webdriver.Chrome("D:\\chromedriver.exe")
 driver.implicitly_wait(5)
 driver.get("https://vtopreg.vit.ac.in/adddrop/")
 
@@ -52,6 +54,7 @@ def sign_out():
     signout_button = driver.find_element_by_xpath("//button[contains(text(),'Sign out')]")
     driver.execute_script("arguments[0].scrollIntoView();", signout_button)
     signout_button.click()
+
 def back_home():
     driver.implicitly_wait(5)
     time.sleep(1)
@@ -59,32 +62,49 @@ def back_home():
     driver.execute_script("arguments[0].scrollIntoView();", home_button)
     home_button.click()
     time.sleep(2)
-def course_type(rb,pg_num):                 #Enter the respective page number to visit in the course type
+def course_type(rb,pg_num):
+    global ue
     driver.implicitly_wait(5)
-    radio_button = driver.find_element_by_xpath(f"//input[@id='registrationOption{rb}']")  #Values of rb can be given as PC = 0 ; PE = 1 ; UC = 2 ; UE = 3
+    radio_button = driver.find_element_by_xpath(f"//input[@id='registrationOption{rb}']")  #PC = 0 ; PE = 1 ; UC = 2 ; UE = 3
     driver.execute_script("arguments[0].scrollIntoView();", radio_button)
     radio_button.click()
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     driver.find_element_by_xpath("//button[contains(text(),'Submit')]").click()
-    pg_id = f"//body/div[@id='page-wrapper']/div[1]/div[1]/div[1]/form[1]/div[1]/div[1]/div[1]/ul[1]/li[{int(pg_num) + 1}]/a[1]"
-    driver.find_element_by_xpath(pg_id).click()
-    time.sleep(2)
+    ue = rb
+    if int(rb) == 3:
+        search_button= "//input[@id='searchValue']"
+        course_code = input("Enter the course code:")
+        driver.find_element_by_xpath("//input[@id='searchValue']").send_keys(course_code)
+        driver.find_element_by_xpath("//button[@id='findBtn']").click()
+
+    else:
+        pg_id = f"//body/div[@id='page-wrapper']/div[1]/div[1]/div[1]/form[1]/div[1]/div[1]/div[1]/ul[1]/li[{int(pg_num) + 1}]/a[1]"
+        driver.find_element_by_xpath(pg_id).click()
+        time.sleep(2)
 def course_reg(name,pg,n):
     #n is the serial number of course on respective page
-    #pg is the respective page number to visit
-    #For name- Enter the name of the course(Any name you want to enter)
+    #For UE courses,pg = 1 and n = 4
     print()
     print(f" {name}:-")
     discord_normal_ping(f" {name}:-")
     driver.implicitly_wait(5)
-    reg_button_number = (((int(pg)*5)-1) - (5-int(n)))
-    button_id = (f"//button[@id='registerbtn_{reg_button_number}']")
-    time.sleep(1)
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    driver.find_element_by_xpath(button_id).click()
-    time.sleep(1)
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(2)
+    if ue == 3:
+        button_id = "//button[@id='registerbtn_0']"
+        time.sleep(1)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        driver.find_element_by_xpath(button_id).click()
+        time.sleep(1)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)
+    else:
+        reg_button_number = (((int(pg)*5)-1) - (5-int(n)))
+        button_id = (f"//button[@id='registerbtn_{reg_button_number}']")
+        time.sleep(1)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        driver.find_element_by_xpath(button_id).click()
+        time.sleep(1)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)
     try:
         faculty_reg(4)
 
@@ -99,17 +119,21 @@ def course_reg(name,pg,n):
             driver.find_element_by_xpath("//button[contains(text(),'Go Back')]").click()
         pass
 def course_view(name,pg,n):
-    # n is the serial number of course on respective page
-    # pg is the respective page number to visit
-    # For name- Enter the name of the course(Any name you want to enter)
     print()
     print(f" {name}:-")
     discord_normal_ping(f" {name}:-")
     driver.implicitly_wait(10)
-    driver.find_element_by_xpath(f"//body[1]/div[3]/div[1]/div[1]/div[1]/form[1]/div[1]/div[{int(pg)+1}]/table[1]/tbody[1]/tr[{n}]/td[12]/button[1]").click()
-    time.sleep(1)
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(1)
+    if ue == 3:
+        driver.find_element_by_xpath("//tbody/tr[1]/td[12]/button[1]").click()
+        time.sleep(1)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(1)
+
+    else:
+        driver.find_element_by_xpath(f"//body[1]/div[3]/div[1]/div[1]/div[1]/form[1]/div[1]/div[{int(pg)+1}]/table[1]/tbody[1]/tr[{n}]/td[12]/button[1]").click()
+        time.sleep(1)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(1)
     try:
         faculty_view(4)
 
@@ -123,7 +147,7 @@ def course_view(name,pg,n):
             time.sleep(1)
             driver.find_element_by_xpath("//button[contains(text(),'Go Back')]").click()
         pass
-def faculty_reg(m):     #m =  selected faculty serial number on the page
+def faculty_reg(m):     #m =  selected faculty number
     driver.implicitly_wait(10)
     a = f"//thead/tr[{int(m)+3}]/td[5]"
     seats1 = driver.find_element_by_xpath(a)
@@ -151,7 +175,7 @@ def faculty_reg(m):     #m =  selected faculty serial number on the page
     print(f"Course Registration Successful: {slot}")
     for _ in range(5): discord_important_ping(f"Course Registration Successful: {slot}")
     driver.get_screenshot_as_file(f"course_reg_ger_{slot}.png")
-def faculty_view(n):    #n =  selected faculty serial number on the page
+def faculty_view(n):
     driver.implicitly_wait(10)
     a = f"//thead/tr[{int(n) + 1}]/td[8]"
     fcname = f"//thead/tr[{int(n) + 1}]/td[4]"
@@ -184,7 +208,7 @@ def process_flow():
 login()
 discord_online()
 driver.maximize_window()
-course_type(1,2)
+course_type(3,1)
 
 i=1
 try:
@@ -207,9 +231,3 @@ try:
 except:
     for _ in range(10): discord_important_ping('Process Failed due to some error:Aborted')
     sign_out()
-
-
-
-
-
-
